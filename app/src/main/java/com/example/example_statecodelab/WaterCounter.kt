5d1a4f0fd8d4c5a2284b868b07a09930c40a2839
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-09-19 17:05:29 -0400

    update for finish

--- app/src/main/java/com/example/example_statecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/example_statecodelab/WaterCounter.kt
@@ -1,16 +1,40 @@
 package com.example.example_statecodelab
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
-   val count = 0
-   Text(
-       text = "You've had $count glasses.",
-       modifier = modifier.padding(16.dp)
-   )
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(
+        count = count,
+        onIncrement = { count++ },
+        modifier = modifier
+    )
+}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            enabled = count < 10,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add one")
+        }
+    }
 }
